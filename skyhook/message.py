20fcddba25481091b8ccb9cc6c4d77435118c257
Oliver Ainsworth <ottajay@googlemail.com>
2021-05-18 19:31:01 +0100

    Add helper for implementing Lambda message receivers

--- skyhook/message.py
+++ skyhook/message.py
@@ -1,5 +1,8 @@
 """Tools for interacting with messages."""
 
+import functools
+import json
+
 import jsonschema
 
 import skyhook.error
@@ -11,6 +14,7 @@ class Messenger:
     def __init__(self, service, name):
         self._service = service
         self._message = service.message(name)
+        self.lambda_ = MessengerLambda(self)
 
     def validate(self, message):
         validator = self._service.validator(self._message.schema)
@@ -23,3 +27,59 @@ class Messenger:
         self.validate(message)  # misplaced?
         hook = skyhook.Hook.current()
         hook.send(self._message.name, message)
+
+
+class MessengerLambda:
+    """For building Lambda implementations of message receivers."""
+
+    def __init__(self, messenger):
+        self._messenger = messenger
+
+    def __call__(self, callable_):
+        """See :meth:`wrap`."""
+        return self.wraps(callable_)
+
+    def wraps(self, callable_):
+
+        @functools.wraps(callable_)
+        def lambda_guard(event, context):
+            messages = (
+                self._messages_from_sns(event)
+                + self._messages_from_sqs(event)
+            )
+            for message in messages:
+                callable_(message)
+
+        return lambda_guard
+
+    def _messages_from_sns(self, event):
+        messages = []
+        if not isinstance(event, dict):
+            return messages
+        if "Records" in event:
+            for record in event["Records"]:
+                if record.get("EventSource") == "aws:sns":
+                    try:
+                        message = json.loads(record["Sns"]["Message"])
+                    except json.JSONDecodeError as error:
+                        raise skyhook.error.ContractError from error
+                    else:
+                        self._messenger.validate(message)
+                        messages.append(message)
+        return messages
+
+    def _messages_from_sqs(self, event):
+        messages = []
+        if not isinstance(event, dict):
+            return messages
+        if "Records" in event:
+            for record in event["Records"]:
+                if record.get("eventSource") == "aws:sqs":
+                    try:
+                        message = json.loads(record["body"])
+                    except json.JSONDecodeError as error:
+                        raise skyhook.error.ContractError from error
+                    else:
+                        self._messenger.validate(message)
+                        messages.append(message)
+        return messages
